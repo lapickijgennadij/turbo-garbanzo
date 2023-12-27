004ae6e24d5aad11774dc9544e1b83bb8a4c5141
speechx <152909110+lapickijgennadij@users.noreply.github.com>
2023-12-27 21:15:38 +0100

    Create turbo-garbanzo.swift

---
+++ turbo-garbanzo.swift
@@ -0,0 +1,10 @@
+    func updateUIView(_ uiView: UILabel, context: Context) {}
+}
+
+struct StrokeText: View {
+    
+    private let text: Text
+    private let width: CGFloat
+    private let textColor: Color
+    private let strokeColor: Color
+    private let quality: Quality
